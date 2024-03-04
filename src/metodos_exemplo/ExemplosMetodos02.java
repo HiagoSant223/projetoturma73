0482efd3d898d538000653e38222b63be02de84e
Hiago Oliveira <hiagosantos223@gmail.com>
2024-03-04 15:42:42 -0300

    metodos

---
+++ src/metodos_exemplo/ExemplosMetodos02.java
@@ -0,0 +1,10 @@
+package metodos_exemplo;
+
+public class ExemplosMetodos02 {
+	
+	// Criando o método s/ retorno e s/ parametro
+	public static void mensagem() {
+		System.out.println("Mensagem: Hello World");
+	}
+	
+}
